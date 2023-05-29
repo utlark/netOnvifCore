f7f77e9cc2f82342b595c3dc9ee9556c528d558b
Yuriy Zubkov <utlark@ya.ru>
2023-05-29 13:46:10 +0500

    Добавил метод для создания Imaging клиента

--- netOnvifCore/OnvifClientFactory.cs
+++ netOnvifCore/OnvifClientFactory.cs
@@ -1,7 +1,9 @@
 ﻿using System.ServiceModel;
 using System.ServiceModel.Channels;
 using netOnvifCore.DeviceManagement;
+using netOnvifCore.Imaging;
 using netOnvifCore.Media;
+using netOnvifCore.Media2;
 using netOnvifCore.Security;
 using DateTime = System.DateTime;
 
@@ -9,15 +11,27 @@ namespace netOnvifCore;
 
 public static class OnvifClientFactory
 {
-    public static async Task<DeviceClient> CreateDeviceClientAsync(string host, string username, string password)
+    public static async Task<DeviceClient> CreateDeviceClientAsync(string deviceIp, string username, string password)
     {
-        return await CreateDeviceClientAsync(new Uri($"http://{host}/onvif/device_service"), username, password);
+        var binding = CreateBinding();
+        var endpoint = new EndpointAddress(new Uri($"http://{deviceIp}/onvif/device_service"));
+        var device = new DeviceClient(binding, endpoint);
+        var timeShift = await GetDeviceTimeShift(device);
+
+        device = new DeviceClient(binding, endpoint);
+        device.ChannelFactory.Endpoint.EndpointBehaviors.Clear();
+        device.ChannelFactory.Endpoint.EndpointBehaviors.Add(new SoapSecurityHeaderBehavior(username, password, timeShift));
+
+        // Connectivity Test
+        await device.OpenAsync();
+
+        return device;
     }
 
     public static async Task<MediaClient> CreateMediaClientAsync(DeviceClient deviceClient)
     {
         var capabilities = await deviceClient.GetCapabilitiesAsync(new[] { CapabilityCategory.Media });
-        var media        = new MediaClient(CreateBinding(), new EndpointAddress(new Uri(capabilities.Capabilities.Media.XAddr)));
+        var media = new MediaClient(CreateBinding(), new EndpointAddress(new Uri(capabilities.Capabilities.Media.XAddr)));
 
         media.ChannelFactory.Endpoint.EndpointBehaviors.Clear();
         foreach (var endpointEndpointBehavior in deviceClient.ChannelFactory.Endpoint.EndpointBehaviors)
@@ -29,11 +43,10 @@ public static class OnvifClientFactory
         return media;
     }
 
-    public static async Task<MediaClient> CreateMediaClientAsync(string host, string username, string password)
+    public static async Task<Media2Client> CreateMedia2ClientAsync(DeviceClient deviceClient)
     {
-        var deviceClient = await CreateDeviceClientAsync(host, username, password);
         var capabilities = await deviceClient.GetCapabilitiesAsync(new[] { CapabilityCategory.Media });
-        var media        = new MediaClient(CreateBinding(), new EndpointAddress(new Uri(capabilities.Capabilities.Media.XAddr)));
+        var media = new Media2Client(CreateBinding(), new EndpointAddress(new Uri(capabilities.Capabilities.Media.XAddr)));
 
         media.ChannelFactory.Endpoint.EndpointBehaviors.Clear();
         foreach (var endpointEndpointBehavior in deviceClient.ChannelFactory.Endpoint.EndpointBehaviors)
@@ -44,6 +57,30 @@ public static class OnvifClientFactory
 
         return media;
     }
+
+    public static async Task<ImagingPortClient> CreateImagingClientAsync(DeviceClient deviceClient)
+    {
+        var capabilities = await deviceClient.GetCapabilitiesAsync(new[] { CapabilityCategory.Media });
+        var media = new ImagingPortClient(CreateBinding(), new EndpointAddress(new Uri(capabilities.Capabilities.Media.XAddr)));
+
+        media.ChannelFactory.Endpoint.EndpointBehaviors.Clear();
+        foreach (var endpointEndpointBehavior in deviceClient.ChannelFactory.Endpoint.EndpointBehaviors)
+            media.ChannelFactory.Endpoint.EndpointBehaviors.Add(endpointEndpointBehavior);
+
+        // Connectivity Test
+        await media.OpenAsync();
+
+        return media;
+    }
+
+    public static async Task<MediaClient> CreateMediaClientAsync(string deviceIp, string username, string password) => 
+        await CreateMediaClientAsync(await CreateDeviceClientAsync(deviceIp, username, password));
+
+    public static async Task<Media2Client> CreateMedia2ClientAsync(string deviceIp, string username, string password) => 
+        await CreateMedia2ClientAsync(await CreateDeviceClientAsync(deviceIp, username, password));
+
+    public static async Task<ImagingPortClient> CreateImagingClientAsync(string deviceIp, string username, string password) => 
+        await CreateImagingClientAsync(await CreateDeviceClientAsync(deviceIp, username, password));
 
     private static Binding CreateBinding()
     {
@@ -56,29 +93,12 @@ public static class OnvifClientFactory
 
         binding.Elements.Add(new HttpTransportBindingElement
         {
-            AllowCookies           = true,
-            MaxBufferSize          = int.MaxValue,
+            AllowCookies = true,
+            MaxBufferSize = int.MaxValue,
             MaxReceivedMessageSize = int.MaxValue
         });
 
         return binding;
-    }
-
-    private static async Task<DeviceClient> CreateDeviceClientAsync(Uri uri, string username, string password)
-    {
-        var binding   = CreateBinding();
-        var endpoint  = new EndpointAddress(uri);
-        var device    = new DeviceClient(binding, endpoint);
-        var timeShift = await GetDeviceTimeShift(device);
-
-        device = new DeviceClient(binding, endpoint);
-        device.ChannelFactory.Endpoint.EndpointBehaviors.Clear();
-        device.ChannelFactory.Endpoint.EndpointBehaviors.Add(new SoapSecurityHeaderBehavior(username, password, timeShift));
-
-        // Connectivity Test
-        await device.OpenAsync();
-
-        return device;
     }
 
     private static async Task<TimeSpan> GetDeviceTimeShift(Device device)
